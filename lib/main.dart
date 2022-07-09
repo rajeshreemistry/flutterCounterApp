7bbd3a1f46342d9c38f2470e77ce4e800b5f88ca
Trushar Mistry <trusharm@native.software>
2022-07-09 15:27:02 +0530

    Change in Text 3

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
                 // 'You have pushed the button this many times:',
-                'This is the numbers:'),
+                'This is the numbers klklaksalkslk:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
