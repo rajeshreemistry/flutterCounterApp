bcb70aa57cf0443bf4df15ba3c525dec429b1b44
Trushar Mistry <trusharm@native.software>
2022-07-09 15:01:06 +0530

    Change button color

--- lib/main.dart
+++ lib/main.dart
@@ -105,9 +105,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      //This is fron Bhavin Branch
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        backgroundColor: Colors.red,
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
